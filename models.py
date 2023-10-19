c43eb427c5279390abbef93cace90ddec65dd175
Sonny V <sonny@softllama.net>
2023-10-19 12:12:55 +0200

    feat(models): add Access-Control-Allow-Origin: '*' as default header

--- models.py
+++ models.py
@@ -5,6 +5,10 @@ from typing import Any
 ListStr = list[str]
 
 
+def _default_response_headers():
+    return {"Access-Control-Allow-Origin": "*"}
+
+
 @dataclass
 class Request:
     """Represent a Request."""
@@ -23,7 +27,7 @@ class Response:
     body: Any
     status_code: int = 200
     content_type: str = "text/html"
-    headers: dict[str, str] = field(default_factory=dict)
+    headers: dict[str, str] = field(default_factory=_default_response_headers)
 
 
 @dataclass
