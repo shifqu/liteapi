4f78fd7baee834ae1e3e8f753e3f8c1e43ffb1f7
Sonny V <sonny@softllama.net>
2023-12-02 19:50:24 +0100

    feat(errors): return the exception message when calling __str__

--- errors.py
+++ errors.py
@@ -4,3 +4,7 @@ from liteapi.models import JSONResponse
 
 class ApiException(JSONResponse, BaseException):
     """Raised whenever an exception occurred during request/response handling."""
+
+    def __str__(self) -> str:
+        """Return only the exception message as a str representation."""
+        return self.body["message"]
